1ef4027b7d69335cd9147653e42363e4bc130bc4
CharlieMickey <charles.f.mickey@gmail.com>
2023-02-17 11:10:46 -0600

    add controller functions and routes for band model

--- controllers/bands.js
+++ controllers/bands.js
@@ -1,7 +1,43 @@
 const Band = require("../models")
 
-
-
+const create = async (req,res) => {
+  try {
+    const band = await Band.create(req.body)
+    res.status(200).json(band)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+const index = async (req,res) =>{
+  try {
+    const bands = await Band.findAll()
+    res.status(200).json(bands)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+const update = async (req,res) =>{
+  try {
+    const band = await Band.findByPk(req.params.id)
+    band.set(req.body)
+    await band.save()
+    res.status(200).json(band)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+const deleteBand = async (req,res) =>{
+  try {
+    const band = await Band.findByPk(req.params.id)
+    await band.destroy()
+    res.status(200).json(band)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
 module.exports = {
-
+  create,
+  index,
+  update,
+  delete:deleteBand
 }
--- routes/bands.js
+++ routes/bands.js
@@ -1,6 +1,12 @@
 const router = requrie("express").Router()
-const catsCtrl = require("../controllers/bands.js")
+const bandsCtrl = require("../controllers/bands.js")
 
+router.post('/', bandsCtrl.create)
 
+router.get('/', bandsCtrl.index)
+
+router.put('/:id', bandsCtrl.update)
+
+router.delete('/:id', bandsCtrl.delete)
 
 module.exports = router
